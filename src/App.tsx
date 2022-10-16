64b2515f51e70b99e2c620c6d494a6736d1d5669
SVA2021 <sofronov.vit@gmail.com>
2022-10-16 21:48:06 +0400

    upd: style optimization for 4 task show

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Main} from './pages/main';
+import {Second} from './pages/second';
+import {Table} from './pages/table';
 
 function App() {
+
+  const [task, setTask] = useState(0)
+
   return (
     <div className="container">
-      <Main />
+      <div className='changer' >
+        <button onClick={() => setTask(0)} disabled={task === 0}>task 1-1</button>
+        <button onClick={() => setTask(1)} disabled={task === 1}>task 1-2</button>
+        <button onClick={() => setTask(2)} disabled={task === 2}>task 2-1</button>
+        <button onClick={() => setTask(3)} disabled={task === 3}>task 2-2</button>
+      </div>
+      {task === 0 && <Main />}
+      {task === 1 && <Second />}
+      {task === 2 && <Table />}
     </div>
   );
 }
